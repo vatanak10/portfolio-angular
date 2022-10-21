6aeb190acf428def9e33ba3481421398c05b7166
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-10-21 23:27:30 +0700

    :art: add hero + portfolio section, toggle theme function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './modules/hero/hero.component';
+import { PortfolioComponent } from './modules/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
