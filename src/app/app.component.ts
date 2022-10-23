9de20b8d86118a82e1248776fd2697e822fe11f8
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-10-23 11:00:33 +0700

    :sparkles: toggle active btn function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,44 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'portfolio-angular';
 
+  menu = [
+    {
+      id: 'home',
+      name: 'Home',
+      icon: 'fas fa-home',
+    },
+    {
+      id: 'about',
+      name: 'About Me',
+      icon: 'fas fa-user',
+    },
+    {
+      id: 'portfolio',
+      name: 'My Portfolio',
+      icon: 'fas fa-briefcase',
+    },
+    // {
+    //   id: 'blogs',
+    //   name: 'Read Blogs',
+    //   icon: 'fas fa-newspaper',
+    // },
+    {
+      id: 'contact',
+      name: 'Contact Me',
+      icon: 'fas fa-envelope',
+    }
+  ]
+
   toggleTheme() {
     document.body.classList.toggle('light-mode');
+  }
+
+  toggleActiveBtn(e: any) {
+    const controls = document.querySelectorAll('.control');
+    controls.forEach((control) => {
+      control.classList.remove('active-btn');
+    });
+
+    e.target.classList.add('active-btn');
   }
 }
