1929bffae3f28cd94b6ce556946a1778b964fb04
vatanak10 <koeurnsereyvatanak@gmail.com>
2023-01-11 16:33:25 +0700

    :art: add gitlab link closes #6

--- src/app/modules/contacts/contacts.component.ts
+++ src/app/modules/contacts/contacts.component.ts
@@ -3,68 +3,71 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   info = [
     {
       title: 'Email',
       value: 'koeurnsereyvatanak@gmail.com',
-      icon: 'fas fa-envelope'
+      icon: 'fas fa-envelope',
     },
     {
       title: 'Mobile Number',
       value: '+855 98 915 099',
-      icon: 'fas fa-mobile-alt'
+      icon: 'fas fa-mobile-alt',
     },
     {
       title: 'Bachelor',
       value: 'Software Engineering',
-      icon: 'fas fa-user-graduate'
+      icon: 'fas fa-user-graduate',
     },
     {
       title: 'Languages',
       value: 'Khmer, English, Japanese',
-      icon: 'fas fa-globe-africa'
+      icon: 'fas fa-globe-africa',
     },
-    {
-      title: 'Date of Birth',
-      value: 'October 04, 2000',
-      icon: 'fas fa-birthday-cake'
-    },
+    // {
+    //   title: 'Date of Birth',
+    //   value: 'October 04, 2000',
+    //   icon: 'fas fa-birthday-cake'
+    // },
     {
       title: 'Location',
       value: 'Phnom Penh, Cambodia',
-      icon: 'fas fa-map-marker-alt'
-    }
-  ]
+      icon: 'fas fa-map-marker-alt',
+    },
+  ];
 
   socialIcons = [
     {
       title: 'LinkedIn',
       icon: 'fab fa-linkedin-in',
-      link: 'https://www.linkedin.com/in/vatanak10/'
+      link: 'https://www.linkedin.com/in/vatanak10/',
     },
     {
       title: 'GitHub',
       icon: 'fab fa-github',
-      link: 'https://github.com/vatanak10'
+      link: 'https://github.com/vatanak10',
+    },
+    {
+      title: 'GitLab',
+      icon: 'fab fa-gitlab',
+      link: 'https://gitlab.com/vatanak10',
     },
     {
       title: 'Twitter',
       icon: 'fab fa-twitter',
-      link: 'https://twitter.com/vatanak100'
+      link: 'https://twitter.com/vatanak100',
     },
     {
       title: 'Facebook',
       icon: 'fab fa-facebook-f',
-      link: 'https://www.facebook.com/vatanak100/'
+      link: 'https://www.facebook.com/vatanak100/',
     },
   ];
 }
