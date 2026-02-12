f9f69ff7877917f6fe9457445b0c8846b2698fb6
vatanak10 <koeurnsereyvatanak@gmail.com>
2026-02-12 15:51:17 +0700

    feat: work experience - add company logo

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -66,6 +66,7 @@ export class AboutComponent implements OnInit {
       date: 'Jan 2025 - Present',
       position: 'Senior Frontend Developer',
       company: 'AIBODIA',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHzxqYvqbk7XWpsEvfkxbyaR4_cLF6fO8NuA&s',
       duties: [
         'Develop and maintain user-facing features using latest frontend technologies.',
         'Optimize applications for maximum speed and scalability.',
@@ -77,6 +78,7 @@ export class AboutComponent implements OnInit {
       date: 'June 2023 - May 2025',
       position: 'Frontend Developer',
       company: 'Youding',
+      logo: 'https://media.licdn.com/dms/image/v2/D560BAQHbeiEtmt1m1A/company-logo_200_200/company-logo_200_200/0/1704957057573/youding_logo?e=2147483647&v=beta&t=BToifYOS-nDk8TjATQHt7nDxaglltBfVp5aqBe9U6Dk',
       duties: [
         'Write reusable, effective, and scalable TypeScript code.',
         'Implement front-end components with a progressive Vue.js framework.',
@@ -88,6 +90,7 @@ export class AboutComponent implements OnInit {
       date: 'January 2023 - June 2023',
       position: 'Frontend Developer',
       company: 'Kirirom Digital Cambodia',
+      logo: 'https://media.licdn.com/dms/image/v2/C560BAQFKCsUioFiBPg/company-logo_200_200/company-logo_200_200/0/1679989931415/kirirom_digital_cambodia_logo?e=2147483647&v=beta&t=O9nrLHzelpou9QsPB1ANrlkcQBpZt2bKCy6kosCain8',
       duties: [
         'Designed and updated layouts to meet usability and performance requirements.',
         'Transformed native applications into mobile-friendly products.',
@@ -98,6 +101,7 @@ export class AboutComponent implements OnInit {
       date: 'June 2022 - January 2023',
       position: 'Web Application Developer',
       company: 'Bongthom.com',
+      logo: 'https://play-lh.googleusercontent.com/wdam3aaKtVTWByzuYRl4XlcdRwZ3fWqxh_SrO9U0SlAP8bVX0qF_3zO_MV8zGja7R3A',
       duties: [
         'Present and defend work and key milestone deliverables to the development team, management, and stakeholders',
         'Web Application Development. Build reusable code and libraries for future use.',
@@ -108,6 +112,7 @@ export class AboutComponent implements OnInit {
       date: 'October 2021 - June 2022',
       position: 'Junior Web Developer',
       company: 'DOOD LIMITED',
+      logo: 'https://media.licdn.com/dms/image/v2/D4E0BAQHUlqOGg0CS5g/company-logo_200_200/company-logo_200_200/0/1707215283106/dood_limited_logo?e=2147483647&v=beta&t=BDLdp34WagV14qNAewhe-ayJ90uMeQutOUmnWXPecVM',
       duties: [
         'Provided front-end website development using WordPress, Hubspot, and other editing software.',
         'Drafted customized layout and wireframes based on predetermined parameters and expectations.',
@@ -118,6 +123,7 @@ export class AboutComponent implements OnInit {
       date: 'December 2021 - April 2022',
       position: 'Web Application Developer',
       company: 'KIT',
+      logo: 'https://static.wixstatic.com/media/f81f50_53b2aa541fdb4b00ba68cfc8eb59aaa0~mv2.png/v1/fill/w_978,h_921,al_c/f81f50_53b2aa541fdb4b00ba68cfc8eb59aaa0~mv2.png',
       duties: [
         'Identify the Problems and Propose the solution to the problem.',
         'Decide on Technology Stack required to build the Web Application.',
