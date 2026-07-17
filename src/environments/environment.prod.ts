e1e6219338876cb40461078f6569757289e860cb
vatanak10 <koeurnsereyvatanak@gmail.com>
2026-07-17 13:46:54 +0700

    :fire: remove sensitive credential from environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://beiuioqcpfzhczatpsxq.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlaXVpb3FjcGZ6aGN6YXRwc3hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwOTA0MzUsImV4cCI6MjA2NDY2NjQzNX0.BZ-tCFHonx7LoOTVe-CPqh4p9JjUdSfOZGuuuGTJvPc',
+  supabaseUrl: '',
+  supabaseKey: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: 'https://beiuioqcpfzhczatpsxq.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlaXVpb3FjcGZ6aGN6YXRwc3hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwOTA0MzUsImV4cCI6MjA2NDY2NjQzNX0.BZ-tCFHonx7LoOTVe-CPqh4p9JjUdSfOZGuuuGTJvPc',
+  supabaseUrl: '',
+  supabaseKey: '',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
